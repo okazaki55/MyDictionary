b7929c5797752e7198c5bebecde377c1c21d04d9
ABDULLAH FURKAN TOY <furkantoy@hotmail.com>
2021-03-17 06:23:10 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public int Length { get; internal set; }
+
+        public void Add(TKey Id, TValue Name)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];                
+            }
+
+            keys[keys.Length - 1] = Id;
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+                        
+            values[values.Length - 1] = Name;
+        }
+        
+    }    
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sozcukler = new MyDictionary<int, string>();
+            sozcukler.Add(1, "Elma");
+            sozcukler.Add(2, "Armut");
+            sozcukler.Add(3, "Portakal");
+            sozcukler.Add(4, "Mandalina");
+
+            foreach (string sozluk in sozcukler)
+            {
+                Console.WriteLine(sozluk);
+            }
+
+            Console.WriteLine(sozcukler.Length);           
+            
+        }
+    }
+}
